7f7356172fcbb01a62e2b985ac6dd14fca51cde1
jwtay <legocrazy00@gmail.ccom>
2024-02-29 20:39:50 -0700

    database structure

---
+++ Mission08_Team0115/Models/EFQuadrantRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace Mission08_Team0115.Models
+{
+    public class EFQuadrantRepository : IQuadrantRepository
+    {
+        private Mission08QuadrantsContext _context;
+        public EFQuadrantRepository(Mission08QuadrantsContext temp)  
+        {
+            _context = temp;
+        }
+
+        public List<Quadrant> Quadrants => _context.Quadrants.ToList();
+
+        public void AddTask(Quadrant quadrant) 
+        {
+            _context.Add(quadrant);
+            _context.SaveChanges();
+        }
+    }
+}
